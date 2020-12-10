75c74868a8fb4374d8c7b66d7ffda520381e5564
Theo Lo <theo.lo@oocl.com>
2020-12-10 18:35:37 +0800

    [todo][Theo]test: added get all todo unit test

--- src/main/java/service/TodoService.java
+++ src/main/java/service/TodoService.java
@@ -1,4 +1,17 @@
 package service;
 
+import model.Label;
+import model.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import repository.TodoRepository;
+
+import java.util.List;
+
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
 }
--- src/test/java/com/thoughtworks/todolist/TodoServiceTest.java
+++ src/test/java/com/thoughtworks/todolist/TodoServiceTest.java
@@ -1,4 +1,45 @@
 package com.thoughtworks.todolist;
 
+import model.Label;
+import model.Todo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import repository.LabelRepository;
+import repository.TodoRepository;
+import service.LabelService;
+import service.TodoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TodoServiceTest {
+    @InjectMocks
+    TodoService todoService;
+
+    @Mock
+    TodoRepository todoRepository;
+
+    private final String labelId = "1";
+
+    @Test
+    void should_return_all_todos_when_get_all_given_all_todos() {
+        //given
+        final List<Todo> expected = new ArrayList<>();
+        expected.add(new Todo());
+        expected.add(new Todo());
+        when(todoRepository.findAll()).thenReturn(expected);
+
+        //when
+        final List<Todo> actual = todoService.getTodos();
+
+        //then
+        assertEquals(expected, actual);
+    }
 }
